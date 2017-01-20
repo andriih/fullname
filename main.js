e23510071e0a26d9fd8395df226f82a5585624fd
Andrii Hnatyshyn <andrii.hnatyshyn@gmail.com>
2017-01-20 15:06:15 +0200

    finish

--- main.js
+++ main.js
@@ -1,22 +1,14 @@
-// (function(){
-//     var fname = document.getElementById("fname").value;
-//     var lname = document.getElementById("lname").value;
-//     var res = document.getElementById("res").innerHTML;
-//     if(fname != "" && lname != ""){
-//         res += "Hello , "+lname;
-//     }
-// })();
+ 
 
-
-document.getElementById("pname").addEventListener("click",function(){
+document.getElementById("lname","fname").addEventListener("blur",function(){
     var fname = document.getElementById("fname").value;
     var lname = document.getElementById("lname").value;
     var res = document.getElementById("res");
     if(fname != "" && lname != ""){
         res.innerHTML = "Hello , "+lname+" "+fname+"<br />";
 
-        fname.value = '';
-        lname.value = "";
+        document.getElementById("fname").value = "";
+        document.getElementById("lname").value = "";
     }
 
 });
